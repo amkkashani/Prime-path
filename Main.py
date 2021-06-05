08d54c9d8160efbdf10e11d0bd0d73e7628e07d5
kashani <amkkashani@gmail.com>
2021-06-05 19:56:07 +0430

    completed

--- Main.py
+++ Main.py
@@ -1,50 +1,136 @@
-
-
 intialNodes = [0]
-vertices = [(0,1),(0,2),(3,1),(1,3),(2,3),(3,4)]
+vertices = [(0, 1), (0, 2), (3, 1), (1, 3), (2, 3), (3, 4)]
 verticesDictionary = {}
 endNodes = [4]
 numberOfTheNodes = 5
 
 stack = []
-results = set()
+results = []
+
 
 def main():
     setupGraph()
-    print(verticesDictionary)
-    DFS(0)
-    print(results)
-    # for i in range(numberOfTheNodes):
-    #     DFS(i)
+    for i in range(numberOfTheNodes):
+        DFS(i)
+    removeSubpaths()
+
+    print("prime paths")
+    for element in results:
+        print(element.passed)
+
+    print("prime path and test path")
+    for element in results:
+        path = element.passed
+        if path[0] in intialNodes and path[-1] in endNodes:
+            print(path)
 
 
-#this code is not popular dfs :)
-#its custom dfs for finding all the ways
+
+# this code is not popular dfs :)
+# its custom dfs for finding all the ways
+def removeSubpaths():
+    i = 0
+    while i < len(results):
+        j = 0
+        while j < len(results):
+            if i >= j :
+                j +=1
+                continue
+            if checkItsSubpath(results[i],results[j]):# deletion during iterations caueses  the complexity inside this block
+                if len(results[i].passed) > len(results[j].passed):
+                    results.remove(results[j])
+                    if j < i:
+                        j -= 1
+                        i -= 1
+                    else:
+                        j -= 1
+
+                else:
+                    results.remove(results[i])
+                    if i < j:
+                        j-=1
+                        i-=1
+                    else:
+                        i-=1
+                    break  # we change the main element and must break
+            j +=1
+        i+=1
+def removeSubpaths2():
+    i = 0
+    j = 0
+    for i in results:
+        for j in results:
+            if i == j:
+                continue
+            if checkItsSubpath(i,j):
+                if len(i.passed) > len(j.passed):
+                    results.remove(j)
+                else:
+                    results.remove(i)
+
+
+
+
+
+def checkItsSubpath(path1, path2):
+    listBig = path1.passed
+    listLit = path2.passed
+
+    if len(listBig) < len(listLit):
+        temp = listLit
+        listLit = listBig
+        listBig = temp
+
+    for i in range(len(listBig)):
+        isSubpath = True
+        for j in range(len(listLit)):
+            if i + j > len(listBig) - 1 or listBig[i + j] != listLit[j]:
+                isSubpath = False
+                break
+        if isSubpath:
+            return True
+    return False
+
+def test():
+    listBig = [0,1,3,4]
+    listLit = [1,3]
+    for i in range(len(listBig)):
+        isSubpath = True
+        for j in range(len(listLit)):
+            if i + j > len(listBig) - 1 or listBig[i + j] != listLit[j]:
+                isSubpath = False
+                break
+        if isSubpath:
+            return True
+    return False
+
+
+
 def DFS(startIndex):
     startNode = path()
-    _,startNode = startNode.addNodeToPath(startIndex)
+    _, startNode = startNode.addNodeToPath(startIndex)
     stack.append(startNode)
 
-    while len(stack)!= 0:
+    while len(stack) != 0:
         currentNode = stack.pop()
         if currentNode.passed[-1] in verticesDictionary:
             canBeTheEndNod = False
             for nextNode in verticesDictionary[currentNode.passed[-1]]:
-                #todo check conditions
-                cond , newPath = currentNode.addNodeToPath(nextNode)
+                cond, newPath = currentNode.addNodeToPath(nextNode)
                 if cond:
                     stack.append(newPath)
                 else:
-                    if newPath !=None:
-                        results.add(newPath)
+                    if newPath != None:
+                        results.append(newPath)
                     else:
                         canBeTheEndNod = True
 
             if currentNode.haveChild == False and canBeTheEndNod:
-                results.add(currentNode)
+                results.append(currentNode)
         else:
             # in this case you are in end of path
-            results.add(currentNode)
+            results.append(currentNode)
+
 
 def setupGraph():
     for tup in vertices:
@@ -58,10 +144,9 @@ class path:
     def __init__(self):
         self.passed = []
         self.collection = set()
-        self.haveChild = False # this field show us that the Node have bigger child path or not
+        self.haveChild = False  # this field show us that the Node have bigger child path or not
 
-
-    def addNodeToPath(self,nodeNumber):
+    def addNodeToPath(self, nodeNumber):
         if nodeNumber in self.collection:
             if self.passed[0] == nodeNumber:
                 newPath = path()
@@ -70,9 +155,9 @@ class path:
                 newPath.passed.append(nodeNumber)
                 newPath.collection.add(nodeNumber)
                 self.haveChild = True
-                return False , newPath
+                return False, newPath
             else:
-                return False , None
+                return False, None
         else:
             newPath = path()
             newPath.passed = self.passed.copy()
@@ -81,8 +166,6 @@ class path:
             newPath.collection.add(nodeNumber)
             self.haveChild = True
             return True, newPath
-
-
 
 
 if __name__ == '__main__':
